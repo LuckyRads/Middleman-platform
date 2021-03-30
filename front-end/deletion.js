0d1cdf598f1fb75f1fefe40fe959125633940605
Elitelol <58183375+Elitelol@users.noreply.github.com>
2021-03-31 00:43:14 +0300

    Updated HTML, added new delete functionality
    Atnaujintas HTML puslapis, pridėtas naujas funkcionalumas su pašalinimu.

--- front-end/deletion.js
+++ front-end/deletion.js
@@ -1,3 +1,17 @@
-function itemDelete(){
-    console.log("Item deleted");
+function itemDelete(element){
+    let a = document.getElementsByClassName(element)[0];
+
+    if(a === undefined){
+        return "Item doesn't exist";
+    }
+    let status = a.childNodes[3];
+
+    if(status.innerHTML === "Status: Deleted"){
+        return "Item is already deleted";
+    }
+
+    status.innerHTML = "Status: Deleted";
+
+    return "Item deleted";
 }
+
