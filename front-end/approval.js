82e9b0a9e27da6d279e5d2621c0b31f99b40f30a
Jegor Klimentjev <jegor.klimentjev@mediapark.com>
2021-04-06 17:57:27 +0300

    feat: jasmine tests added;

--- front-end/approval.js
+++ front-end/approval.js
@@ -3,34 +3,35 @@ let approvedTransactions = [];
 
 function approve(element){
     if(approvedTransactions.includes(element)){
-        return "Transaction is already approved";
+        return "Transaction is already approved.";
     }
 
     if(!transactions.includes(element)){
-        return "Transaction doesn't exist";
+        return "Transaction doesn't exist.";
     }
 
     approvedTransactions.push(element);
 
-    return "Transaction approved";
+    return "Transaction approved.";
 }
 
-function loadTransactions(){
+function load(){
     let i = 0;
 
     while (i < 6) {
-        transactions.push(10000 + 1)
+        transactions.push(10000 + i)
 
         i++;
     }
 }
 
-function clearApproved(){
+function clear(){
     approvedTransactions = [];
+    transactions = [];
 }
 
 module.exports = {
     approve,
-    loadTransactions,
-    clearApproved
+    load,
+    clear,
 }
